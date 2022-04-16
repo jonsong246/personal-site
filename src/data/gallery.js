ff592ddc4ae2fb93df90ac80879073a100f1583f
Jon Song <jonsong246@gmail.com>
2022-04-15 18:34:27 -0700

    nutrients logo replaced

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -1,12 +1,44 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Deez Nutrients',
+    title: 'Living Room - Day',
+    subtitle: 'Personal Projects',
+    image: '/images/gallery/Render_AssignmentA.png',
+    date: '2018-12-30',
+    desc:
+      '3D Rendered Living Room Timelapse',
+  },
+  {
+    title: 'Living Room - Evening',
+    subtitle: 'Personal Projects',
+    image: '/images/gallery/Render_AssignmentB.png',
+    date: '2018-12-30',
+    desc:
+      '3D Rendered Living Room Timelapse',
+  },
+  {
+    title: 'Cabin - Day',
     subtitle: 'Personal Projects',
     image: '/images/gallery/Cabin_A.png',
     date: '2018-12-30',
     desc:
-      'A Nutrition tracking application',
+      '3D Rendered Cabin Timelapse',
+  },
+  {
+    title: 'Cabin - Evening',
+    subtitle: 'Personal Projects',
+    image: '/images/gallery/Cabin_B.png',
+    date: '2018-12-30',
+    desc:
+      '3D Rendered Cabin Timelapse',
+  },
+  {
+    title: 'Gummy Bears',
+    subtitle: 'Personal Projects',
+    image: '/images/gallery/GummyBears',
+    date: '2018-12-30',
+    desc:
+      '3D Rendered Gummy Bears',
   },
 ];
 
