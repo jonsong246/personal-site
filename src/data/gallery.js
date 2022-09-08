70d6563d3fbc8c708071636b8b3f7370925bddeb
Jon Song <jonsong246@gmail.com>
2022-09-08 00:05:49 -0700

    new art for gallery

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -6,7 +6,7 @@ const data = [
     image: '/images/gallery/Render_AssignmentA.jpg',
     date: '2018-12-30',
     desc:
-      '3D Rendered Living Room Timelapse',
+      '3D Rendered Living Room Timelapse built with Autodesk Maya',
   },
   {
     title: 'Living Room - Evening',
@@ -14,7 +14,7 @@ const data = [
     image: '/images/gallery/Render_AssignmentB.jpg',
     date: '2018-12-30',
     desc:
-      '3D Rendered Living Room Timelapse',
+      '3D Rendered Living Room Timelapse built with Autodesk Maya',
   },
   {
     title: 'Cabin - Day',
@@ -22,7 +22,7 @@ const data = [
     image: '/images/gallery/Cabin_A.png',
     date: '2018-12-30',
     desc:
-      '3D Rendered Cabin Timelapse',
+      '3D Rendered Cabin Timelapse built with Autodesk Maya',
   },
   {
     title: 'Cabin - Evening',
@@ -30,15 +30,87 @@ const data = [
     image: '/images/gallery/Cabin_B.jpg',
     date: '2018-12-30',
     desc:
-      '3D Rendered Cabin Timelapse',
+      '3D Rendered Cabin Timelapse built with Autodesk Maya',
   },
   {
-    title: 'Gummy Bears',
+    title: 'Still Life 1',
     subtitle: 'Personal Projects',
-    image: '/images/gallery/GummyBears.jpg',
-    date: '2018-12-30',
+    image: '/images/gallery/01_Still_Life.jpg',
+    date: '2014-05-30',
     desc:
-      '3D Rendered Gummy Bears',
+      'Still Life drawing using charcoal pencil',
+  },
+  {
+    title: 'Still Life 2',
+    subtitle: 'Personal Projects',
+    image: '/images/gallery/02_Still_Life.jpg',
+    date: '2014-05-30',
+    desc:
+      'Still Life drawing using charcoal pencil',
+  },
+  {
+    title: 'Still Life 3',
+    subtitle: 'Personal Projects',
+    image: '/images/gallery/03_Still_Life.jpg',
+    date: '2014-05-30',
+    desc:
+      'Still Life drawing using charcoal pencil',
+  },
+  {
+    title: 'Still Life 4',
+    subtitle: 'Personal Projects',
+    image: '/images/gallery/04_Still_Life.jpg',
+    date: '2014-05-30',
+    desc:
+      'Still Life drawing using charcoal pencil and watercolor',
+  },
+  {
+    title: 'Deep in Thought',
+    subtitle: 'Personal Projects',
+    image: '/images/gallery/Deep_in_thought.jpg',
+    date: '2014-05-30',
+    desc:
+      'Medium: Charcoal pencil and watercolor',
+  },
+  {
+    title: 'Notes to Dance',
+    subtitle: 'Personal Projects',
+    image: '/images/gallery/07_Notes_to_Dance.jpg',
+    date: '2014-05-30',
+    desc:
+      'Medium: Charcoal pencil and watercolor',
+  },
+  {
+    title: 'Demonata Concept Art',
+    subtitle: 'Personal Projects',
+    image: '/images/gallery/Demonata_Concept.jpg',
+    date: '2014-05-30',
+    desc:
+      'My conceptual rendition of characters from the Demon Ata book series - Tablet (Adobe Photoshop)',
+  },
+  {
+    title: 'Catcher in the Rye Concept Art',
+    subtitle: 'Personal Projects',
+    image: '/images/gallery/Catcher_in_the_Rye.jpg',
+    date: '2014-05-30',
+    desc:
+      'My conceptual rendition of a book cover for Catcher in the Rye - Adobe Illustrator',
+  },
+  {
+    title: 'LACMA Poster',
+    subtitle: 'Personal Projects',
+    image: '/images/gallery/15_LACMA_Postcard.jpg',
+    date: '2014-05-30',
+    desc:
+      'My conceptual rendition of a poster for the LACMA Exhibition - Adobe Illustrator',
+  },
+  {
+    title: 'Saving Tree Poster',
+    subtitle: 'Personal Projects',
+    image: '/images/gallery/12_Saving_Tree_Poster.jpg',
+    date: '2014-05-30',
+    desc:
+      'My conceptual rendition of a poster for the World Wildlife Organization - Adobe Illustrator',
   },
 ];
 
